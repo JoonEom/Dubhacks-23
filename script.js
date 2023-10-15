96779d39f57d39d869211216d25baaf732263944
Minjoon Eom <eomminjoon81@gmail.com>
2023-10-15 00:11:28 -0700

    fuck javascript

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 function getFlightNum(event) {
     event.preventDefault();
     let flightNum = document.getElementById("flight-number").value;
-    console.log(flightNum);
-    window.location.href = 'match.html'
+    window.location.href = "match.html";
+}
+
+
+function getMatches() {
+    const Http = new XMLHttpRequest();
+    const url='https://us-west1-festive-airway-393617.cloudfunctions.net/dbgetusers';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+         return Http.responseText;
+    }
+    return Http.responseText;
 }
 
 function getLogin(event) {
     event.preventDefault();
     let username = document.getElementById("username").value;
     let password = document.getElementById("login-password").value;
-    console.log(username);
-    console.log(password);
-    window.location.href = 'home.html'
+    window.location.href = 'home.html';
+
+    
 }
 
 function getSignup(event) {
@@ -20,10 +32,6 @@ function getSignup(event) {
     let lastname = document.getElementById("lastname").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("signup-password").value;
-    console.log(firstname);
-    console.log(lastname);
-    console.log(email);
-    console.log(password);
-    window.location.href = 'login.html'
-
+    window.location.href = 'login.html';
+    
 }
